121a4f20ed74950bd281352c3255dee6208f084d
bowmanjeffs <bowmanjs@gmail.com>
2014-03-24 11:36:00 -0700

    add filter_seqs_7
    v7 fixes several major issues in v5, which was including seqs from
    outside filter region and not eliminating all gap-only columns

--- filter_seqs_7.py
+++ filter_seqs_7.py
@@ -18,8 +18,11 @@ v5 allows sequences in alignment to take up multiple lines
 import re
 import sys
 
-file_in = sys.argv[1]
-#file_in = 'DUF900_pro_aligned.fasta'  ## use this for testing
+try:
+    file_in = sys.argv[1]
+except IndexError:
+    file_in = 'Bac_luciferase_pro_aligned.fasta'  ## use this for testing
+
 name = re.sub('.fasta', '', file_in)
 log = open(name+'.filter.log', 'w')
 
@@ -34,30 +37,21 @@ print >> log, '\n',
 ## find start and end for each sequence
                     
 start_end = {}
-max_starts = []
-min_ends = []
 starts = set()
 ends = set()
 
 print 'filtering to last start and first end'
 print >> log, 'filtering to last start and first end'
 
-l = 0
-
 with open(file_in, 'r') as fasta_in:
+    l = 0
+    seqname = ''
     lines = ''
+    
     for line in fasta_in:
-        
-        if line.startswith('>') == False:
-            line = line.rstrip()
-            lines = lines + line
-            
-        else:
-            l = l + 1
-            if l != 1:
+        if line.startswith('>') == True:
+            if l != 0:
                 rlines = lines[::-1]
-                
-                l = l + 1
                 
                 for i,p in enumerate(lines):
                     if p not in gap_character:
@@ -69,16 +63,22 @@ with open(file_in, 'r') as fasta_in:
                         end = len(rlines) - i
                         break
                                 
-                print l, start, end
-                print >> log, l, start, end
-                start_end[l] = start, end
+                print l, seqname, start, end
+                print >> log, l, seqname, start, end
+                start_end[seqname] = start, end
                 starts.add(start)
                 ends.add(end)
                 lines = ''
+            l = l + 1    
+            seqname = line.rstrip()
+            seqname = seqname.strip('>')
+            
+        else:
+            line = line.rstrip()
+            lines = lines + line
+
 
 ## make sure you get the last line!
-
-l = l + 1
 
 for i,p in enumerate(lines):
     if p not in gap_character:
@@ -88,49 +88,62 @@ for i,p in enumerate(lines):
 for i,p in enumerate(rlines):
     if p not in gap_character:
         end = len(rlines) - i
-        break
+        break 
                 
 print l, start, end
-print >> log, l, start, end
-start_end[l] = start, end
+print >> log, l, seqname, start, end
+start_end[seqname] = start, end
 starts.add(start)
 ends.add(end)
-            
+
+## now you've finished with the last line
+           
 min_end = min(ends)
-max_start = max(starts)   
+max_start = max(starts)
+
+print >> log, l, 'sequences evaluated'   
 print >> log, 'max start / min end =', max_start, '/', min_end 
 
 ## eliminate late starts and early finishes until minimum alignment length reached
 
-bad = []
+bad = set()
 
 n = 0
 starts = sorted(starts, reverse = True)
 ends = sorted(ends)
 
 while(min_end - max_start) < min_positions:
-    print 'min_end - max_start =', min_end - max_start
-    print >> log, 'max start - min end is below cutoff, eliminating some sequences'        
+    print min_end, '-', max_start, '=', min_end - max_start
+    print >> log, min_end, '-', max_start, '=', min_end - max_start, 'this is below cutoff, eliminating some sequences'        
+    
     for key in start_end.keys():
         if key not in bad:
-            if start_end[key][0] == max_start:
-                bad.append(key)
-            elif start_end[key][1] == min_end:
-                bad.append(key)
+                
+            if start_end[key][0] <= max_start:
+                if start_end[key][1] >= min_end:
+                    continue
+                else:
+                    bad.add(key)
+                    print >> log, key, 'is bad', start_end[key][0], start_end[key][1]
+            else:
+                bad.add(key)
+                print >> log, key, 'is bad', start_end[key][0], start_end[key][1]
                 
     min_end = ends[n]
     max_start = starts[n]
     n = n + 1
+    
+print >> log, 'new max start / min end =', max_start, '/', min_end  
  
 ## look for gap only columns among good sequences
 
 gap = {}
        
 with open(file_in, 'r') as fasta_in:
+    seqname = ''
     nseq = 0
     lines = ''
     for line in fasta_in:
-        
         if line.startswith('>') == False:
             line = line.rstrip()
             lines = lines + line
@@ -138,10 +151,8 @@ with open(file_in, 'r') as fasta_in:
         else:
             nseq = nseq + 1
             if nseq != 1:
-            
-                nseq = nseq + 1
-                if nseq not in bad:
-                    print nseq
+                if seqname not in bad:
+                    print nseq, seqname, 'is good'
                     
                     for i,p in enumerate(lines):
                         if p in gap_character:
@@ -151,76 +162,78 @@ with open(file_in, 'r') as fasta_in:
                                 gap[i] = temp
                             except KeyError:
                                 gap[i] = 1
-                lines = ''
-                
-nseq = nseq + 1
-if nseq != 1:
+            lines = ''
+            seqname = line.rstrip()
+            seqname = seqname.strip('>')
 
-    nseq = nseq + 1
-    if nseq not in bad:
-        print nseq
-        
-        for i,p in enumerate(lines):
-            if p in gap_character:
-                try:
-                    temp = gap[i]
-                    temp = temp + 1
-                    gap[i] = temp
-                except KeyError:
-                    gap[i] = 1
+
+if seqname not in bad:
+    print nseq, seqname
+    
+    for i,p in enumerate(lines):
+        if p in gap_character:
+            try:
+                temp = gap[i]
+                temp = temp + 1
+                gap[i] = temp
+            except KeyError:
+                gap[i] = 1
                             
-## generate filter and remove gaps
+## determine which positions are used
                     
-flter = {}
+p_use = set()
 
-for key in range(0, len(lines)):
-    if key < max_start:
-        flter[key] = 0
-    elif key > min_end:
-        flter[key] = 0
-    elif key in gap.keys():
-        if gap[key] == nseq - len(bad): 
-            flter[key] = 0
-        else:
-            flter[key] = 1
-    else:
-        flter[key] = 1
-
-flter_list = []        
-for key in flter.keys():
-    if flter[key] == 1:
-        flter_list.append(key)
+for i in range(0, max(ends)):
+    if i >= max_start:
+        if i <= min_end:
+            if gap[i] < (len(start_end.keys()) - len(bad)):
+                p_use.add(i)
             
 with open(file_in, 'r') as fasta_in, open(name+'.filter.fasta', 'w') as fasta_out:
-    l = 0
     seq = ''
+    seq_out = ''
+    keep = False
+    
     for line in fasta_in:
         if line.startswith('>'):
-            if l != 0:
-                seq_out = ''
+
+            if keep == True:
+                print seqname, 'generating filtered seq'
                 for i,p in enumerate(seq):
-                    if flter[i] == 1:
+                    if i in p_use:
                         seq_out = seq_out + p
                 print >> fasta_out, seq_out
-            l = l + 1
-            seq = ''
-            if l not in bad:
-                print >> fasta_out, line,
+                seq = ''
+                seq_out = ''
+            
+            seqname = line.rstrip()
+            seqname = seqname.strip('>')
+            
+            if seqname in bad:
+                print >> log, seqname, 'not added to filtered fasta'
+                keep = False
+            
             else:
-                line = line.strip()
-                print >> log, line+' is bad'
+                keep = True
+                seq = ''
+                print >> log, seqname, 'is added to filtered fasta'
+                print >> fasta_out, line,
         
-        else:
-            if l not in bad:
-                line = line.rstrip('\n')
-                seq = seq + line
-    print >> fasta_out, seq_out
+        elif keep == True:
+            line = line.rstrip()
+            seq = seq + line
                 
 print >> log, '\n'
 print >> log, 'filter:'
-            
-for key in flter.keys():
-    print >> log, flter[key],
+ 
+flter = ''           
+for i in range(0, max(ends)):
+    if i in p_use:
+        flter = flter + '1'
+    else:
+        flter = flter + '0'
+
+print >> log, flter
 
 log.close()
                 
